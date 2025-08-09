e318f3265d455f5602ddb381a24315b6b8c1e127
tomxi1997 <tomxi1997@gmail.com>
2025-08-09 14:54:55 +0800

    2st commit

--- user_config.sh
+++ user_config.sh
@@ -83,7 +83,7 @@ GCC_VERSION=
 
 
 ##以下几乎不休改
-sudo docker build \
+sudo docker buildx build \
              --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
              --build-arg KERNEL_SOURCE=${KERNEL_SOURCE} \
              --build-arg KERNEL_BRANCH=${KERNEL_BRANCH} \
